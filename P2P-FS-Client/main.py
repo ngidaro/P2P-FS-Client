5faeeeed6e7da48ba316660e8d0f8c90b3254371
Nicholas <ngidaro@hotmail.com>
2021-11-04 15:29:12 -0400

    Cleanup registering

--- P2P-FS-Client/main.py
+++ P2P-FS-Client/main.py
@@ -18,6 +18,8 @@ def send_data_to(s, msg, host, port):
         addr = d[1]
 
         print('Server Reply: ' + reply + '\n')
+
+        return reply
     except socket.error:
         print('Error')
 
@@ -26,6 +28,7 @@ def start_UDP_connection():
     client_host = ''  # Can be '0.0.0.0'
     client_port_UDP = 0  # Can be 8889
     client_port_TCP = 0  # Can be 10000
+    isBound = 0  # True if the socket has bound
     name = ''
 
     # UDP Socket
@@ -49,17 +52,24 @@ def start_UDP_connection():
             cleanup_de_register(s, host, port, name)
             s.close()
             sys.exit()
+        # If name does not exist then the user has not Registered
+        elif not name:
+            client_host, client_port_UDP, client_port_TCP, name = pc.get_data(msg)
+
+            if not isBound:
+                s.bind((client_host, client_port_UDP))
+                isBound = 1
+
+            response = send_data_to(s, msg, host, port)
+
+            # Require user to re-register if it has been denied
+            if response.split(' ')[0] == 'REGISTER-DENIED':
+                name = ''
+
+        elif msg.split(' ')[0] == 'REGISTER':
+            print('Client is already Registered')
         else:
-            # Save the clients IP and ports on the client side
-            if not client_host and not client_port_UDP and not client_port_TCP:
-                client_host, client_port_UDP, client_port_TCP, name = pc.get_data(msg)
-                if client_host and client_port_UDP and client_port_TCP and name:
-                    s.bind((client_host, client_port_UDP))
-                    send_data_to(s, msg, host, port)
-            elif msg.split(' ')[0] == 'REGISTER':
-                print('Client is already Registered')
-            else:
-                send_data_to(s, msg, host, port)
+            send_data_to(s, msg, host, port)
 
 
 if __name__ == '__main__':
